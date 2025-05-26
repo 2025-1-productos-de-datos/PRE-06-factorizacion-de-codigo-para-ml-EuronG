f5d67134eeb7089d952f34ec0ff673992d3852a3
EuronG <juandigiraldoj@gmail.com>
2025-05-25 19:06:51 -0500

    gitnore

--- homework/src/internals/save_model.py
+++ homework/src/internals/save_model.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 
-def save_model(model, save_path="models/estimatos.pkl"):
+def save_model(model, save_path="models/estimator.pkl"):
     if not os.path.exists("models"):
         os.makedirs("models")
     with open(save_path, "wb") as file:
